8ceb99b492cbbca58510a816e5c1e4532f173cb6
Rounak Basak <rounakbasak12@gmail.com>
2024-05-12 19:37:12 +0530

    vanvar compleate with button color change

--- src/components/header/MobilePage.jsx
+++ src/components/header/MobilePage.jsx
@@ -3,8 +3,8 @@ import React from 'react'
 function MobilePage({ name, icon }) {
     return (
         <span className='text-xl flex items-center hover:bg-slate-300 dark:hover:bg-slate-800 justify-center py-2 border-b-[1px] border-black dark:border-white font-semibold cursor-pointer duration-100 ease-linear hover:text-yellow-500'>
-            <div className='flex items-center'>
-                <p className='mb-[5px] '>{icon}</p>
+            <div className='flex items-center gap-1'>
+                <p className='mb-[5px] text-yellow-500'>{icon}</p>
                 <p>{name}</p>
 
             </div>
