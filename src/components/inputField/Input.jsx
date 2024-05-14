9566750590f2c80fd109ee0ac25e8ca1e52c68b0
Rounak Basak <rounakbasak12@gmail.com>
2024-05-14 23:29:45 +0530

    hero section add input

--- src/components/inputField/Input.jsx
+++ src/components/inputField/Input.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 
-function Input() {
+function Input({design}) {
     return (
 
-        <form>
+      <div className={`${design}`}>
             <label for="search" className="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">Search</label>
             <div className="relative">
                 <div className="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
@@ -14,7 +14,7 @@ function Input() {
                 <input type="search" id="search" className="block outline-none border-none w-full p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search Cryptocurrency" required />
                 <button type="submit" className="text-white absolute end-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Search</button>
             </div>
-        </form>
+            </div>
 
     )
 }
